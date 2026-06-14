a39e1a66af1cafa178ff0224e0d539b6f7855e94
maymaewa <maymaewa@gmail.com>
2026-06-14 23:50:15 +0300

    hw2 completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,34 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+	var builder strings.Builder
+	length := len(runes)
+
+	for i := 0; i < length; i++ {
+		r := runes[i]
+
+		if unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+
+		count := 1
+		next := i+1
+
+		if next < length && unicode.IsDigit(runes[next]) {
+			count = int(runes[next] - '0')
+			i++
+		}
+
+		builder.WriteString(strings.Repeat(string(r), count))
+	}
+
+	return builder.String(), nil
 }
