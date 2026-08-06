1d9a811d4f9afe0e3ddca82b4761545cd93e5494
maymaewa <maymaewa@gmail.com>
2026-08-06 22:30:10 +0300

    hw08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,56 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env.
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return 1
+	}
+
+	current := make(map[string]string)
+
+	for _, e := range os.Environ() {
+		parts := strings.SplitN(e, "=", 2)
+
+		if len(parts) == 2 {
+			current[parts[0]] = parts[1]
+		}
+	}
+
+	for key, value := range env {
+		if value.NeedRemove {
+			delete(current, key)
+			continue
+		}
+
+		current[key] = value.Value
+	}
+
+	envList := make([]string, 0, len(current))
+
+	for k, v := range current {
+		envList = append(envList, k+"="+v)
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Env = envList
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	err := command.Run()
+	if err == nil {
+		return 0
+	}
+
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		return exitErr.ExitCode()
+	}
+
+	return 1
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,23 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	code := RunCmd(
+		[]string{"go", "version"},
+		Environment{},
+	)
+
+	if code != 0 {
+		t.Fatalf("expected exit code 0, got %d", code)
+	}
+}
+
+func TestRunCmd_CommandNotFound(t *testing.T) {
+	code := RunCmd(
+		[]string{"not-existing-command"},
+		Environment{},
+	)
+
+	if code == 0 {
+		t.Fatal("expected non-zero exit code")
+	}
 }
