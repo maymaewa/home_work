999265bab6a40d5785d1ab81a0b17d585253d49d
maymaewa <maymaewa@gmail.com>
2026-08-17 20:04:42 +0300

    hw08 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "usage: go-envdir envdir command [args...]")
+		os.Exit(1)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	returnCode := RunCmd(os.Args[2:], env)
+
+	os.Exit(returnCode)
 }
